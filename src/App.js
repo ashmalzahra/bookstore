50a145816023c47e0b02a7b905f78be2d608d3df
ashmalzahra <ashmal.zahraa@gmail.com>
2023-02-25 01:51:47 +0500

    Create Navbar

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import './App.css';
+import { Routes, Route } from 'react-router-dom';
+import Categories from './routes/categories';
+import Books from './routes/books';
 
 function App() {
   return (
-    <></>
+    <Routes>
+      <Route index element={<Books />}/>
+      <Route  path="categories" element={<Categories />}/>
+    </Routes>
   );
 }
 
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,24 @@
+import { NavLink } from 'react-router-dom';
+
+const links = [
+    { path: '/', text: 'Books' },
+    { path: 'categories', text: 'Categories' },
+  ];
+
+const Navbar = () => {
+    return(
+        <nav>
+            <ul>
+            {links.map((link) => (
+            <li key={link.text}>
+              <NavLink to={link.path}>
+                {link.text}
+              </NavLink>
+            </li>
+          ))}
+            </ul>
+        </nav>
+    )
+}
+
+export default Navbar
