70922d85f3359ba60b9238bd9228d355c6b7dde0
ashmalzahra <ashmal.zahraa@gmail.com>
2023-03-02 03:48:44 +0500

    Display books

--- src/routes/books.jsx
+++ src/routes/books.jsx
@@ -1,11 +1,20 @@
 import Book from '../components/Books';
 import Form from '../components/Form';
+import { useSelector } from 'react-redux';
 
-const Books = () => (
+const Books = () => {
+  const {books} = useSelector((state) => state.cart);
+
+  return(
   <div>
-    <Book />
+    <ul>
+    {books.map((book) => {
+      return <Book key={book.item_id} title={book.title} author={book.author}/>
+    })}
+    </ul>
     <Form />
   </div>
-);
+  )
+};
 
 export default Books;
