885835e1209145ba5298bc35a3539fb4c472bc3c
ashmalzahra <ashmal.zahraa@gmail.com>
2023-03-04 14:19:54 +0500

    Refractor categories route according to its style

--- src/routes/categories.jsx
+++ src/routes/categories.jsx
@@ -4,14 +4,18 @@ import { status } from '../redux/categories/categoriesSlice';
 const Categories = () => {
   const dispatch = useDispatch();
   const { categories } = useSelector((state) => state.categories);
-
-  return (
-    <div>
-      <button type="button" onClick={() => dispatch(status())}>Check Status</button>
-      <br />
-      <span>{categories}</span>
-    </div>
-  );
+  let content;
+  if (categories.length > 0) {
+    content = <h2>{categories}</h2>;
+  } else {
+    content = (
+      <button type="button" onClick={() => dispatch(status())}>
+        Check status
+      </button>
+    );
+  }
+  return <section>{content}</section>;
 };
+
 
 export default Categories;
